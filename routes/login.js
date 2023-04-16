6d165e3aa359301fc1002594fa23e3af3801ee94
Michael Guedko <mjgeko@gmail.com>
2023-04-16 11:41:43 -0400

    Add login method

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 /* GET login page. */
 router.get('/', function(req, res, next) {
     res.render('login', { title: 'Login' });
   });
   
-  module.exports = router;
+router.post('/', async (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+    })
+});
+
+module.exports = router;
