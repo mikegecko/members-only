cc6a35b3ad1d0778b3be0d0f5d70252607dbef9f
Michael Guedko <mjgeko@gmail.com>
2023-04-18 13:00:31 -0400

    Add collection to schema

--- models/message.js
+++ models/message.js
@@ -6,7 +6,7 @@ const MessageSchema = new Schema({
     created_at: { type: Date, default: Date.now },
     created_by: { type: Schema.Types.ObjectId, ref: 'User' },
     updated_at: { type: Date, default: Date.now },
-}) 
+}, {collection: 'messages'}); 
 
 
 module.exports = mongoose.model('Message', MessageSchema);
