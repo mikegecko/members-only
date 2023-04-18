af13ef3051ad1f6e91e02947933b0c38d4da53ae
Michael Guedko <mjgeko@gmail.com>
2023-04-18 13:00:03 -0400

    Add message schema

--- models/message.js
+++ models/message.js
@@ -1,2 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+    content: { type: String, required: true },
+    created_at: { type: Date, default: Date.now },
+    created_by: { type: Schema.Types.ObjectId, ref: 'User' },
+    updated_at: { type: Date, default: Date.now },
+}) 
+
+
+module.exports = mongoose.model('Message', MessageSchema);
