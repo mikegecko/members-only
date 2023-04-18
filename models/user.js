3b7e667804d727192fc80d8182ca64b7c9b55aea
Michael Guedko <mjgeko@gmail.com>
2023-04-18 12:56:19 -0400

    Add more properties to user schema

--- models/user.js
+++ models/user.js
@@ -5,6 +5,9 @@ const UserSchema = new Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: false},
+    role: {type: String, required: true},
+    firstName: {type: String, required: false},
+    lastName: {type: String, required: false},
 }, {collection: 'users'});
 
 module.exports = mongoose.model('User', UserSchema);
