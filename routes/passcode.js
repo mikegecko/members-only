288d2efaf2ddb4c1c120d585a086c0c080edea25
Michael Guedko <mjgeko@gmail.com>
2023-04-18 14:09:50 -0400

    Add logic for changing member role

--- routes/passcode.js
+++ routes/passcode.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const User = require("../models/user");
 
 router.get("/", function (req, res, next) {
   res.render("passcode", { title: "Passcode", user: req.user });
 });
 
-router.post("/", function (req, res, next) {
+router.post("/", async function(req, res, next) {
     if(req.body.passcode === process.env.PASSCODE){
-        //Change user role to 'member'
+        try {
+            const user = await User.findByIdAndUpdate(req.user._id, {
+                role: 'member',
+            });
+            user.role = "member";
+        } catch (error) {
+            return next(error);
+        }
         res.redirect("/");
     } else{
         res.redirect("/passcode");
