093fcd0f6a93ae294d7d26a4e94407f698335beb
Michael Guedko <mjgeko@gmail.com>
2023-04-19 14:29:24 -0400

    Add logic for getting messages from db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const Message = require('../models/message');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home', user: req.user });
+router.get('/', async function(req, res, next) {
+  try {
+    const messages = await Message.find({});
+    res.render('index', { title: 'Home', user: req.user, messages: messages });
+  } catch (error) {
+    return next(error)
+  }
 });
 
 module.exports = router;
